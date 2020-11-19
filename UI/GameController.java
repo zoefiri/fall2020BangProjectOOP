f36faa7c5ef838f50b0af265345a1568a57dd067
coleroper26 <coleroper26@gmail.com>
2020-11-18 22:49:44 -0600

    Add-Players-Tab

--- UI/GameController.java
+++ UI/GameController.java
@@ -4,16 +4,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
 import javax.xml.soap.Text;
 
 public class GameController {
-    public Button btnTemp;
+    public int bots;
+
+    public Polygon polyOct;
+    private static final String tableURL = "/UI/table.jpg";
 
     public void init(int b) {
-        System.out.println("Create game with " + b + " bots.");
-        this.btnTemp.setText(String.valueOf(b));
+        this.bots = b;
+        Image table = new Image(tableURL);
+        polyOct.setFill(new ImagePattern(table));
     }
 
 
@@ -24,6 +31,6 @@ public class GameController {
         stage.setTitle("Bang! The Dice Game");
         FinishController gc = loader.getController();
         stage.show();
-        ((Stage)btnTemp.getScene().getWindow()).close();
+        ((Stage)polyOct.getScene().getWindow()).close();
     }
 }
--- UI/StartController.java
+++ UI/StartController.java
@@ -22,5 +22,17 @@ public class StartController {
         gc.init((int)sliderBots.getValue());
         stage.show();
         ((Stage)btnPlay.getScene().getWindow()).close();
+        /**
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Game.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Bang! The Dice Game");
+        GameController gc = loader.getController();
+        System.out.println(gc);
+        gc.init((int)sliderBots.getValue());
+        //gc.init(3);
+        stage.show();
+        ((Stage)btnPlay.getScene().getWindow()).close();
+         */
     }
 }
