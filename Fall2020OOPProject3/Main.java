09644d0231edfc1833e3ca758646c928008bff88
Christian Wells <1cjawesomedude@gmail.com>
2020-12-03 00:20:22 -0600

    Add Main.java

---
+++ Fall2020OOPProject3/Main.java
@@ -0,0 +1,22 @@
+package Fall2020OOPProject3;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("UI/Start.fxml"));
+        primaryStage.setTitle("Bang! The Dice Game");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
