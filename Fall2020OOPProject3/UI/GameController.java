64bb9b4bd27e2355c2cdde1a399d3ff9bbd20452
coleroper26 <coleroper26@gmail.com>
2020-12-04 23:13:52 -0600

    Add Click to Shoot and Tips
    Tips is located at the bottom of the "Roll" tab.
    Lambda listener added on each rectangle containing the player images.

--- Fall2020OOPProject3/UI/GameController.java
+++ Fall2020OOPProject3/UI/GameController.java
@@ -164,37 +164,31 @@ public class GameController {
      * @param exp2 Undead or Alive expansion enabled
      */
     public void init(int bots, boolean exp1, boolean exp2) {
-
         System.setOut(new PrintStream(System.out) {
             public void println(String s) {
                 historyTextArea.appendText(s + System.lineSeparator());
             }
-
             public void println() {
                 historyTextArea.appendText(System.lineSeparator());
                 historyTextArea.setScrollTop(Double.MAX_VALUE);
             }
-
             public void print(String s) {
                 historyTextArea.appendText(s);
                 historyTextArea.setScrollTop(Double.MAX_VALUE);
             }
         });
-
         // set up discovered roles table
         colCharacter.setCellValueFactory(new PropertyValueFactory<>("character"));
         colHealth.setCellValueFactory(new PropertyValueFactory<>("CurrentHPMask"));
         colRole.setCellValueFactory((new PropertyValueFactory<>("RoleMask")));
-
+        // set up game
         game = new Game(bots, exp1, exp2);      // set up game
         polyOct.setFill(imgpatTable);           // set up table
         updPlayers();                           // set up player character cards
         updArrows();                            // set up arrows
-
         humanRole = game.players.get(0).getRole().toString();
-
+        // set up dice
         imgDie = new ImageView[]{imgDie1, imgDie2, imgDie3, imgDie4, imgDie5};
-
         black = new ColorAdjust();
         black.setHue(-1);
         black.setSaturation(.25);
@@ -236,19 +230,31 @@ public class GameController {
                 System.out.println();
                 System.out.println(game.players.get(i).getCharacter() + "'s turn");
                 game.takeComputerTurn(game.players.get(i));
-                //updPlayers();
             }
             rollCount = 1;
             System.out.println();
             historyTextArea.appendText("");
             updPlayers();
         });
+
+        // Lambda listener on character images for shooting
+        Rectangle[] rectChars = {this.rectPlayer, this.rectBot1, this.rectBot2, this.rectBot3, this.rectBot4, this.rectBot5, this.rectBot6, this.rectBot7};
+        EventHandler<MouseEvent> shootListener = (MouseEvent e) -> {
+            if (e.getSource() instanceof Rectangle) {
+                int ind = 0;
+                for (int i = 0; i < rectChars.length; i++)
+                    if (rectChars[i] == e.getSource()) ind = i;
+                game.players.get(0).shootPlayer(game.players.get(ind));
+                if (game.players.get(ind).isEliminated()) game.handleElim(game.players.get(ind));
+                updPlayers();
+            }
+        };
+        for (Rectangle r: rectChars) r.setOnMouseClicked(shootListener);
     }
         
 
     /**
      * Update Players and Roles and Details tabs to reflect players still alive
-     *
      */
     private void updPlayers() {
         if (game.players.size() == 0)
@@ -286,7 +292,6 @@ public class GameController {
 
     /**
      * Update arrow art to reflect the number of arrows held by each player or bot
-     *
      */
     private void updArrows() {
         Circle[] play = {this.circPlayer1, this.circPlayer2, this.circPlayer3, this.circPlayer4, this.circPlayer5, this.circPlayer6, this.circPlayer7, this.circPlayer8, this.circPlayer9};
@@ -311,7 +316,10 @@ public class GameController {
             }
         }
     }
-    
+
+    /**
+     * Updates dice images on Roll tab
+     */
     public void updateDiceImages(){
         try {
             for (int i = 0; i < imgDie.length; i++) {
@@ -395,13 +403,15 @@ public class GameController {
             resolveDice();
             return;
         }
-        // updPlayers();
     }
 
     ArrayList<Player> targets;
     int shot1Left= 0;
     int shot2Left = 0;
-            
+
+    /**
+     * Act upon the dice chosen by players on Roll tab
+     */
     public void resolveDice(){
         Player player = game.players.get(0);
         for(Die d: game.dice) {
